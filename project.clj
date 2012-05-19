4ac2bb540753b7623bdf41aa1d742bb40d98fcd3
Hugo Duncan <hugo@hugoduncan.org>
2012-05-18 22:23:23 -0400

    Rename artifact to pallet/lein-template
    Lein 2 will automatically resolve this artifact name when invoked with
    'lein new pallet'

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cloudhoist/lein-pallet-new "0.1.1-SNAPSHOT"
+(defproject pallet/lein-template "0.1.1-SNAPSHOT"
   :description "Leiningen plugin for creating Pallet projects"
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
