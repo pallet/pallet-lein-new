08834f5884f3044d3c23ea176c43fc0155077804
Hugo Duncan <hugo@hugoduncan.org>
2012-06-21 10:07:10 -0400

    Update to 0.2.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pallet/lein-template "0.2.1"
+(defproject pallet/lein-template "0.2.2-SNAPSHOT"
   :description "Leiningen plugin for creating Pallet projects"
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
