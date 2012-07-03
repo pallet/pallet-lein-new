1217b6bc5723a4f23c23da37f42282c38f7a6496
Hugo Duncan <hugo@hugoduncan.org>
2012-07-02 22:10:36 -0400

    Add url and license to project.clj

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0-beta2"]]}}
   :checksum-deps true
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :url "https://github.com/pallet/pallet-lein-new"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"})
